e85a2797be8b8995be235defdae00ef12073b40f
Chelsea Brodin <brodinchelsea@gmail.com>
2022-10-11 21:11:54 -0500

    Refactored forecast and added useEffect to pull correct data to match weather

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,18 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Forecast.css";
 import axios from "axios";
 import ForecastDay from "./ForecastDay";
+import { cleanup } from "@testing-library/react";
 
 
 export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [wetforecast, setWetforecast] = useState (null);
 
+useEffect(() =>{
+  setLoaded(false);
+},[props.coord]);
 
 function handleResponse(response){
 setWetforecast(response.data.daily);
   setLoaded (true);
 }
+function load(){   let apiKey = "40ee9c494fa4d6774c1dda0bb71d8806";
+let longitude = props.coord.lon;
+let latitude = props.coord.lat;
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(handleResponse);}
 
  if (loaded) {
    return (
@@ -37,13 +47,7 @@ setWetforecast(response.data.daily);
      </div>
    );
  } else {
-   let apiKey = "40ee9c494fa4d6774c1dda0bb71d8806";
-   let longitude = props.coord.lon;
-   let latitude = props.coord.lat;
-   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-   
-   axios.get(apiUrl).then(handleResponse);
-
+load ();
    return null;
 }
  }
